dfe8ec5c6aa2db0794cebcb683ddbbc445e1a82d
Samuel Olekšák <soleksak@redhat.com>
2024-04-27 22:30:12 +0200

    Block content hashing

--- block.py
+++ block.py
@@ -2,8 +2,10 @@
 # The analysis of cryptographic techniques for offloading computations and storage in blockchains
 # Master thesis 2023/24
 # Samuel Olekšák
-# ✔️❌❌❌❌
+# ✔️✔️❌❌❌
 # ####################################################################################################
+
+import hashlib
 
 from block_header import BlockHeader
 from coin_tx import CoinTransaction
@@ -14,11 +16,29 @@ class Block:
     __coin_txs: list[CoinTransaction]
     __proof_txs: list[ProofTransaction]
 
-    def __init__(self, serial_id, state_root_hash):
-        pass
+    def __init__(self, header, coin_txs, proof_txs):
+        self.__header = header
+        self.__coin_txs = coin_txs
+        self.__proof_txs = proof_txs
 
     def hash_coin_txs(self):
-        pass
+        tx_hashes = []
+
+        for tx in self.__coin_txs:
+            tx_hashes.append(tx.hash())
+        
+        serialized_txs = tx_hashes.encode()
+        return hashlib.sha256(serialized_txs).digest()
     
     def hash_proof_txs(self):
-        pass
+        tx_hashes = []
+
+        for tx in self.__proof_txs:
+            tx_hashes.append(tx.hash())
+        
+        serialized_txs = tx_hashes.encode()
+        return hashlib.sha256(serialized_txs).digest()
+
+    def finalize_block(self):
+        self.__header.set_txs_hashes(self.hash_coin_txs(), self.hash_proof_txs())
+        self.__header.calculate_hash()
--- block_header.py
+++ block_header.py
@@ -2,8 +2,10 @@
 # The analysis of cryptographic techniques for offloading computations and storage in blockchains
 # Master thesis 2023/24
 # Samuel Olekšák
-# ✔️❌❌❌❌
+# ✔️✔️❌❌❌
 # ####################################################################################################
+
+import hashlib
 
 class BlockHeader:
     __serial_id: int
@@ -13,10 +15,21 @@ class BlockHeader:
     __previous_block_hash: bytes # SHA256 hash (32 bytes)
     __current_block_hash: bytes  # SHA256 hash (32 bytes)
     __coin_txs_hash: bytes       # SHA256 hash (32 bytes)
-    __proof_txs: bytes           # SHA256 hash (32 bytes)
+    __proof_txs_hash: bytes      # SHA256 hash (32 bytes)
 
-    def __init__(self, serial_id, state_root_hash):
-        pass
+    def __init__(self, serial_id, state_root_hash, timestamp, difficulty, previous_block_hash):
+        self.__serial_id = serial_id
+        self.__state_root_hash = state_root_hash
+        self.__timestamp = timestamp
+        self.__difficulty = difficulty
+        self.__previous_block_hash = previous_block_hash
 
-    def hash(self):
-        pass
+    def calculate_hash(self):
+        serialized_block = [self.__serial_id, self.__state_root_hash, self.__timestamp, self.__difficulty, self.__previous_block_hash, self.__coin_txs_hash, self.__proof_txs_hash].encode()
+        block_hash = hashlib.sha256(serialized_block).digest()
+
+        self.__current_block_hash = block_hash
+
+    def set_txs_hashes(self, coin_txs_hash, proof_txs_hash):
+        self.__coin_txs_hash = coin_txs_hash
+        self.__proof_txs_hash = proof_txs_hash
