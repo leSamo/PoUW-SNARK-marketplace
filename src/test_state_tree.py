377889ed84a3b873d052161444166571093f7fd4
Samuel Olekšák <soleksak@redhat.com>
2024-05-01 21:14:48 +0200

    Add state tree tests

---
+++ src/test_state_tree.py
@@ -0,0 +1,88 @@
+import pytest
+
+from state_tree import StateTree
+
+def test_insert():
+    st = StateTree()
+    st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 123)
+
+def test_insert_invalid_key_type():
+    with pytest.raises(TypeError):
+        st = StateTree()
+        st.set("123", 123)
+
+def test_insert_invalid_key_length():
+    with pytest.raises(ValueError):
+        st = StateTree()
+        st.set(bytes.fromhex("1234"), 123)
+
+def test_insert_invalid_value_type():
+    with pytest.raises(TypeError):
+        st = StateTree()
+        st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), "invalid")
+
+def test_insert_negative_value():
+    with pytest.raises(ValueError):
+        st = StateTree()
+        st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), -123)
+
+def test_retrieve():
+    st = StateTree()
+    st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 123)
+    value = st.get(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"))
+
+    assert value == 123
+
+def test_retrieve_missing():
+    st = StateTree()
+    st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 123)
+    value = st.get(bytes.fromhex("4568b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f123"))
+
+    assert value == 0    
+
+def test_retrieve_invalid_key_type():
+    with pytest.raises(TypeError):
+        st = StateTree()
+        st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 123)
+        st.get("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2")
+
+def test_retrieve_invalid_key_length():
+    with pytest.raises(ValueError):
+        st = StateTree()
+        st.get(bytes.fromhex("1234"))
+
+def test_update():
+    st = StateTree()
+    st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 123)
+    st.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 456)
+    value = st.get(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"))
+
+    assert value == 456
+
+def test_encode_decode():
+    st1 = StateTree()
+    st1.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 123)
+    st1.set(bytes.fromhex("4568b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f123"), 456)
+
+    encoded = st1.encode()
+    
+    st2 = StateTree()
+    st2.decode(encoded)
+    value1 = st2.get(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"))
+    value2 = st2.get(bytes.fromhex("4568b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f123"))
+    value3 = st2.get(bytes.fromhex("0008b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f000"))
+
+    assert value1 == 123
+    assert value2 == 456
+    assert value3 == 0
+
+    st2.set(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"), 234)
+    st2.set(bytes.fromhex("7778b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f777"), 789)
+
+    value1 = st2.get(bytes.fromhex("0318b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f6f2"))
+    value2 = st2.get(bytes.fromhex("7778b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f777"))
+    value3 = st2.get(bytes.fromhex("0008b58b73bbfd6ec26f599649ecc624863c775e034c2afea0c94a1c0641d8f000"))
+
+    assert value1 == 234
+    assert value2 == 789
+    assert value3 == 0
