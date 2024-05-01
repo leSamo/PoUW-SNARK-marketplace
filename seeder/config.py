c71aaa719e0d2b23a1b4f935fa0dd79abdf8de23
Samuel Olekšák <soleksak@redhat.com>
2024-05-01 21:15:07 +0200

    Remove seeder

--- seeder/config.py
+++
@@ -1,9 +0,0 @@
-# ####################################################################################################
-# The analysis of cryptographic techniques for offloading computations and storage in blockchains
-# Master thesis 2023/24
-# Samuel Olekšák
-# ❌❌❌❌❌
-# ####################################################################################################
-
-SEEDER_HOST = 'localhost'
-SEEDER_PORT = 2222
