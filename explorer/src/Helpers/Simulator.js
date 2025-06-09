edfded792c6715e5465d363d7c29edf58c9e32ee
Samuel Olekšák <soleksak@redhat.com>
2025-06-09 23:13:55 +0200

    Simulation progress

--- explorer/src/Helpers/Simulator.js
+++ explorer/src/Helpers/Simulator.js
@@ -1,6 +1,34 @@
+export const EVENT_TYPE = {
+    // called on connected nodes
+    RECEIVE_PEER: "RECEIVE_PEER",
+    RECEIVE_BLOCK: "RECEIVE_BLOCK",
+    RECEIVE_PENDING_COIN_TX: "RECEIVE_PENDING_COIN_TX",
+    RECEIVE_PENDING_PROOF_TX: "RECEIVE_PENDING_PROOF_TX",
+
+    // called by nodes on themselves
+    CONFIRM_COIN_TX: "CONFIRM_COIN_TX",
+    CONFIRM_PROOF_TX: "CONFIRM_PROOF_TX",
+}
+
+const Event = {
+    scheduledTime: 0,
+    type: null,
+    recipient: null,
+    payload: null
+}
+
 const Simulator = {
     randomSeed: 0,
     stopTime: 1000,
-    nodes: [],
-    edges: []
+    currentTime: 0,
+    initialState: {
+        nodes: [],
+        edges: [],
+        pendingEvents: []
+    },
+    currentState: {
+        nodes: [],
+        edges: [],
+        pendingEvents: []
+    }
 };
--- explorer/src/Helpers/SimulatorNode.js
+++ explorer/src/Helpers/SimulatorNode.js
@@ -1,3 +1,5 @@
+import { EVENT_TYPE } from "./Simulator";
+
 SIMULATOR_NODE_STATE = {
     INACTIVE: "INACTIVE",
     EAVESDROP: "EAVESDROP",
@@ -11,5 +13,11 @@ const SimulatorNode = {
     pendingProofTxs: [],
     knownBlocks: [],
     state: SIMULATOR_NODE_STATE.INACTIVE,
-    performance: 0
+    performance: 0,
+    executeEvent: (eventType) => { // returns array of event to be scheduled
+        switch (eventType) {
+            case EVENT_TYPE.RECEIVE_PEER:
+                break;
+        }
+    }
 };
